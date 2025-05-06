4aaab5e8b402ce9bf45e079c6e2e8111bf6e82b4
hilmynugroho <hilmynugroho1@gmail.com>
2025-05-06 22:38:23 +0700

    vid capture

---
+++ video-capture.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import os
+
+video_path = "C:\Users\ideapad\Downloads\train_video.mp4"
+output_dir = 'dataset/images/train'
+os.makedirs(output_dir, exist_ok=True)
+
+cap = cv.VideoCapture(video_path)
+frame_rate = 1  # 1 frame per second
+
+fps = int(cap.get(cv.CAP_PROP_FPS))
+interval = fps * frame_rate
+frame_count = 0
+img_count = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    if frame_count % interval == 0:
+        filename = os.path.join(output_dir, f'frame_{img_count:04d}.jpg')
+        cv.imwrite(filename, frame)
+        img_count += 1
+    frame_count += 1
+
+cap.release()
